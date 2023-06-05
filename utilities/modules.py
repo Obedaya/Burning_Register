6e31d2680d317a2006f4b8dd6ff0ce1665f7d4dd
Lukas Holzner <holzner@fs.tum.de>
2023-06-06 00:19:17 +0200

    fix(): movies selcetion
    Co-authored-by: Obedaya <Obedaya@users.noreply.github.com>

--- utilities/modules.py
+++ utilities/modules.py
@@ -13,5 +13,6 @@ def create_movie_selection():
             st.session_state["movie"] = []
         else:
             st.session_state["movie"] = movies[0]
-            st.session_state["movie"] = st.selectbox("Select movie", options=movies, index= 0 if "movie" not in st.session_state else movies.index(st.session_state["movie"]))
+    if len(movies) > 0:
+        st.session_state["movie"] = st.selectbox("Select movie", options=movies, index= 0 if "movie" not in st.session_state else movies.index(st.session_state["movie"]))
 
